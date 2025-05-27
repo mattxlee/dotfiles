d8d3aad33261c09228a2e85a78ff005ff4e04f80
MATTHΞW <mattxlee@gmail.com>
2025-05-27 12:21:35 +0900

    Add lsp progress to lualine.

--- nvim/lua/plugins/layouts.lua
+++ nvim/lua/plugins/layouts.lua
@@ -1,13 +1,29 @@
 return {
     {
+        "linrongbin16/lsp-progress.nvim",
+        config = function()
+            require("lsp-progress").setup()
+        end,
+    },
+    {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         opts = {
             options = {
                 icons_enabled = true,
-                component_separators = "",
-                section_separators = "",
+                component_separators = '',
+                section_separators = '',
             },
-        }
+            sections = {
+                lualine_a = { 'mode' },
+                lualine_b = { 'branch', 'diff', 'diagnostics' },
+                lualine_c = { 'filename' },
+                lualine_x = { 'encoding', 'fileformat', 'filetype', function()
+                    return require('lsp-progress').progress()
+                end },
+                lualine_y = { 'progress' },
+                lualine_z = { 'location' }
+            },
+        },
     }
 }
