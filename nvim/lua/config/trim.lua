5cc36ef453d2403d9912e485ba9233fead622f28
MATTHΞW <mattxlee@gmail.com>
2025-08-02 20:33:27 +0900

    fix: setup trim.

--- nvim/lua/config/trim.lua
+++ nvim/lua/config/trim.lua
@@ -1,1 +1,9 @@
+require('trim').setup({
+    ft_blocklist = {},
+    patterns = {
+        [[%s/\(\n\n\)\n\+/\1/]], -- replace multiple blank lines with a single line
+    },
+    trim_on_write = false,
+    highlight = true
+})
 vim.keymap.set("n", "<Leader>x", ":Trim<CR>")
