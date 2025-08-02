4f6c0a49702103c7c0d4d0379f7fd1db38e60e99
MATTHΞW <mattxlee@gmail.com>
2025-08-02 23:16:16 +0900

    fix: replace the shortcut to <leader>tt instead of <c-\\>.

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -2,7 +2,7 @@ return {
     {
         { 'akinsho/toggleterm.nvim', version = "*",
             config = {
-                open_mapping = [[<C-\>]], -- Shortcut to open/close terminal
+                open_mapping = [[<Leader>tt]], -- Shortcut to open/close terminal
                 insert_mappings = true,   -- Apply mapping in insert mode
                 terminal_mappings = true, -- Apply mapping in terminal mode
                 direction = "float",
