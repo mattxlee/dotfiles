4c61ab5c817653b4deff6a5123d01113500ce3ab
MATTHΞW <mattxlee@gmail.com>
2025-08-05 16:37:23 +0900

    fix: use <c-v> to show preview from oil.

--- nvim/lua/plugins/browser.lua
+++ nvim/lua/plugins/browser.lua
@@ -25,8 +25,11 @@ return {
     {
         'stevearc/oil.nvim',
         opts = {
-            columns = {
-            }
+            columns = {},
+            keymaps = {
+                ["<C-v>"] = "actions.preview",
+                ["<C-p>"] = false,
+            },
         },
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
