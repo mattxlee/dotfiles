b22e9e663434636fed4a7345d4ba874f38a60e72
MATTHΞW <mattxlee@gmail.com>
2025-08-08 22:03:21 +0900

    feat: following the file status in neo-tree.

--- nvim/lua/plugins/browser.lua
+++ nvim/lua/plugins/browser.lua
@@ -11,6 +11,8 @@ return {
         opts = {
             close_if_last_window = true,
             filesystem = {
+                follow_current_file = true,
+                use_libuv_file_watcher = true,
                 window = {
                     position = "left",
                     width = 53,
