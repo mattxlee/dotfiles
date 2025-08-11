8c83278da774b00a66ebafa5fbf085adafa90f65
MATTHΞW <mattxlee@gmail.com>
2025-08-12 00:52:32 +0900

    feat: add barbar.

---
+++ nvim/lua/config/barbar.lua
@@ -0,0 +1,7 @@
+vim.keymap.set('n', '<Leader>bn', ':BufferNext<CR>')
+vim.keymap.set('n', '<Leader>bp', ':BufferPrevious<CR>')
+
+vim.keymap.set('n', '<Leader>bb', ':BufferPick<CR>')
+
+vim.keymap.set('n', '<Leader>bx', ':BufferClose<CR>')
+vim.keymap.set('n', '<Leader>bt', ':BufferCloseAllButCurrent<CR>')
