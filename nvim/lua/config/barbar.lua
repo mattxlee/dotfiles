47cb5e127d42d40106424583a4e998021940c7e0
MATTHΞW <mattxlee@gmail.com>
2025-08-16 21:05:47 +0900

    feat: add <leader>br to reset lsp.

--- nvim/lua/config/barbar.lua
+++ nvim/lua/config/barbar.lua
@@ -2,6 +2,7 @@ vim.keymap.set('n', '<Leader>bn', ':BufferNext<CR>')
 vim.keymap.set('n', '<Leader>bp', ':BufferPrevious<CR>')
 
 vim.keymap.set('n', '<Leader>bb', ':BufferPick<CR>')
+vim.keymap.set('n', '<Leader>br', ':LspRestart<CR>')
 
 vim.keymap.set('n', '<Leader>bx', ':BufferClose<CR>')
 vim.keymap.set('n', '<Leader>bt', ':BufferCloseAllButCurrent<CR>')
