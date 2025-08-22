5e20ba2ad9a2724d4cc6d94892e16ff50f29fed7
MATTHΞW <mattxlee@gmail.com>
2025-08-23 00:04:57 +0900

    rm: treesitter.

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,20 +1,5 @@
 return {
     {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        opts = {
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-            sync_install = false,
-            ignore_install = {},
-            highlight = {
-                enable = true,
-                disable = {},
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = false },
-        }
-    },
-    {
         "mason-org/mason.nvim",
         config = {
             ui = {
