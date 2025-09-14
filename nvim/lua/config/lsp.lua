94005bdca999f8ba13d3f58f4191a3a874aed125
MATTHΞW <mattxlee@gmail.com>
2025-09-15 03:27:32 +0900

    fix: use <c-l> to open complete menu instead of <c-space>.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -48,7 +48,7 @@ cmp.setup({
     mapping = cmp.mapping.preset.insert({
         ["<C-u>"] = cmp.mapping.scroll_docs(-4),
         ["<C-d>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-l>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
         ["<CR>"] = cmp.mapping.confirm({ select = auto_select }),
         -- LuaSnip jump mappings
