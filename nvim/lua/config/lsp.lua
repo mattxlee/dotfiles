90e3045e454c54c4bb00783e63242caeef72ab5b
MATTHΞW <mattxlee@gmail.com>
2025-09-14 18:13:34 +0900

    feat: replace to cmp.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -1,3 +1,4 @@
+-- shortcuts
 vim.keymap.set("n", "<Leader>m", ":Mason<CR>")
 vim.keymap.set("n", "<Leader>d", vim.diagnostic.open_float, { desc = "Show diagnostics under cursor" })
 vim.keymap.set("n", "<C-k>", vim.lsp.buf.hover, { desc = "Show hover documentation" })
@@ -5,17 +6,60 @@ vim.keymap.set("n", "<Leader>i", vim.lsp.buf.format, { desc = "Format current do
 vim.keymap.set("n", "<Leader>ca", vim.lsp.buf.code_action, { desc = "Show code actions" })
 vim.keymap.set("n", "<Leader>rn", vim.lsp.buf.rename, { desc = "Rename current symbol" })
 vim.keymap.set("n", "<Leader>rs", ":LspRestart<CR>", { desc = "Restart LSP" })
-vim.keymap.set('n', '<leader>l', function()
+vim.keymap.set("n", "<leader>l", function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { desc = "Toggle inlay hints" })
 
-require('lspconfig').rust_analyzer.setup {
-    settings = {
-        ['rust-analyzer'] = {
-            check = {
-                command = 'clippy',
-                extraArgs = {},
-            },
-        },
+-- mason
+require("mason").setup(
+    {
+        ui = {
+            backdrop = 100,
+        }
+    }
+)
+
+-- mason-lspconfig
+require("mason-lspconfig").setup({
+    ensure_installed = { "lua_ls", "vimls" },
+    automatic_installation = true,
+    automatic_enable = true,
+})
+
+-- cmp
+local cmp = require("cmp")
+cmp.setup({
+    snippet = {
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body)
+        end,
     },
-}
+    window = {
+        completion = cmp.config.window.bordered(),
+        documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-d>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-e>"] = cmp.mapping.abort(),
+        ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" },
+    }, {
+        { name = "path" },
+        { name = "buffer" },
+    })
+})
+cmp.setup.filetype("gitcommit", {
+    sources = cmp.config.sources({
+        { name = "git" },
+    }, {
+        { name = "buffer" },
+    })
+})
+
+-- cmp-git
+require("cmp_git").setup()
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,91 +1,26 @@
 return {
     {
-        "mason-org/mason.nvim",
-        config = {
-            ui = {
-                backdrop = 100,
-            }
-        },
+        "mason-org/mason-lspconfig.nvim",
+        dependencies = {
+            { "mason-org/mason.nvim", opts = {} },
+            "neovim/nvim-lspconfig",
+        }
     },
-    { "mason-org/mason-lspconfig.nvim",  config = true },
+    { "hrsh7th/nvim-cmp" },
+    { "hrsh7th/cmp-nvim-lsp" },
+    { "hrsh7th/cmp-buffer" },
+    { "hrsh7th/cmp-path" },
+    { "hrsh7th/cmp-cmdline" },
     {
-        "saghen/blink.cmp",
-        version = "1.*",
-        dependencies = { 'L3MON4D3/LuaSnip', version = 'v2.*' },
-        opts = {
-            keymap = {
-                preset = "none",
-                ["<C-space>"] = { "show", "hide" },
-
-                ["<C-l>"] = { "show", "hide"},
-                ["<C-k>"] = { "show_signature", "hide_signature" },
-
-                ["<C-y>"] = { "select_and_accept" },
-
-                ["<Up>"] = { "select_prev", "fallback" },
-                ["<Down>"] = { "select_next", "fallback" },
-                ["<C-p>"] = { "select_prev", "fallback_to_mappings" },
-                ["<C-n>"] = { "select_next", "fallback_to_mappings" },
-
-                ["<C-u>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-d>"] = { "scroll_documentation_down", "fallback" },
-
-                ["<C-f>"] = { "snippet_forward", "fallback" },
-                ["<C-b>"] = { "snippet_backward", "fallback" },
-            },
-            completion = {
-                menu = {
-                    auto_show = false,
-                },
-                documentation = {
-                    auto_show = true,
-                },
-                ghost_text = { enabled = false },
-            },
-            snippets = { preset = 'luasnip' },
-            sources = {
-                default = { "lsp", "path", "snippets", "buffer" },
-                providers = {
-                    lsp = {
-                        transform_items = function(_, items)
-                            local seen = {}
-                            local deduplicated = {}
-                            for _, item in ipairs(items) do
-                                local key = item.label .. item.kind
-                                if not seen[key] then
-                                    seen[key] = true
-                                    table.insert(deduplicated, item)
-                                end
-                            end
-                            return deduplicated
-                        end,
-                    },
-                }
-            },
-            signature = {
-                enabled = false,
-                window = {
-                    show_documentation = true,
-                },
-            },
-            fuzzy = { implementation = "prefer_rust_with_warning" },
-        },
-        opts_extend = { "sources.default" },
+        "petertriho/cmp-git",
+        dependencies = { 'hrsh7th/nvim-cmp' },
+        init = function()
+            table.insert(require("cmp").get_config().sources, { name = "git" })
+        end
     },
     {
-        "neovim/nvim-lspconfig",
-        dependencies = { "saghen/blink.cmp" },
-        opts = {
-            servers = { -- You can add your own setup here
-                lua_ls = {}
-            }
-        },
-        config = function(_, opts)
-            local lspconfig = require("lspconfig")
-            for server, config in pairs(opts.servers) do
-                config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
-            end
-        end,
-    }
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+        build = "make install_jsregexp"
+    },
 }
