1a7488e748eb5f82745ccc73dfede3103b0349ce
MATTHΞW <mattxlee@gmail.com>
2025-10-03 21:25:49 +0900

    fix: maybe this will improve the order of auto-complete list.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -122,14 +122,32 @@ cmp.setup({
         end, { "i", "s" }),
     }),
     sources = cmp.config.sources({
-        { name = "nvim_lsp_signature_help", group_index = 1 },
+        { name = "copilot",                 group_index = 2 },
+        { name = "nvim_lsp_signature_help", group_index = 2 },
         { name = "nvim_lsp",                group_index = 2 },
         { name = "luasnip",                 group_index = 2 },
-        { name = "copilot",                 group_index = 3 },
     }, {
-        { name = "path", group_index = 1 },
-        -- { name = "buffer", group_index = 1 },
+        { name = "path", group_index = 2 },
+        { name = "buffer", group_index = 2 },
     }),
+    sorting = {
+        priority_weight = 2,
+        comparators = {
+            require("copilot_cmp.comparators").prioritize,
+
+            -- Below is the default comparitor list and order for nvim-cmp
+            cmp.config.compare.offset,
+            -- cmp.config.compare.scopes, --this is commented in nvim-cmp too
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+            cmp.config.compare.recently_used,
+            cmp.config.compare.locality,
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+        },
+    },
 })
 cmp.setup.filetype("gitcommit", {
     sources = cmp.config.sources({
