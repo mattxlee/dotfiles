e919a2eb295106fb185fff4aca8673a9e1710d68
MATTHΞW <mattxlee@gmail.com>
2025-10-05 18:04:12 +0900

    fix: hide copilot suggestion on auto-complete menu popup

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -145,3 +145,11 @@ require("cmp_git").setup()
 
 -- flutter
 require("flutter-tools").setup {} -- use defaults
+
+cmp.event:on("menu_opened", function()
+  vim.b.copilot_suggestion_hidden = true
+end)
+
+cmp.event:on("menu_closed", function()
+  vim.b.copilot_suggestion_hidden = false
+end)
