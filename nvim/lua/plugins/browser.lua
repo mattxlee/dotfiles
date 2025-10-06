162cf2e38ccae73289082ffac1ebb0bfdc59c783
MATTHΞW <mattxlee@gmail.com>
2025-10-06 17:04:17 +0900

    feat(keymap): use gd for LSP definitions in fzf-lua
    Replace <C-]> with gd for triggering lsp_definitions via fzf-lua. This aligns
    with common LSP navigation conventions and improves workflow consistency.

--- nvim/lua/plugins/browser.lua
+++ nvim/lua/plugins/browser.lua
@@ -62,13 +62,13 @@ return {
             )
             require("fzf-lua").register_ui_select()
             vim.keymap.set("n", "<c-p>", ":FzfLua files<CR>")
-            vim.keymap.set("n", "<c-]>", ":FzfLua lsp_definitions<CR>")
             vim.keymap.set("n", "<Leader>l", ":FzfLua grep_cword<CR>")
             vim.keymap.set("n", "<Leader>f", ":FzfLua live_grep<CR>")
             vim.keymap.set("n", "<Leader>z", ":FzfLua diagnostics_workspace<CR>")
             vim.keymap.set("n", "<Leader>o", ":FzfLua lsp_document_symbols<CR>")
             vim.keymap.set("n", "<Leader>g", ":FzfLua lsp_live_workspace_symbols<CR>")
             vim.keymap.set("n", "<Leader>b", ":FzfLua buffers<CR>")
+            vim.keymap.set("n", "gd", ":FzfLua lsp_definitions<CR>")
             vim.keymap.set("n", "gr", ":FzfLua lsp_references<CR>")
         end,
     },
