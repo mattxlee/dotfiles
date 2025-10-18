998d740c9d512eeaae3d2396b60281ccc1a016d3
MATTHΞW <mattxlee@gmail.com>
2025-10-18 17:55:10 +0900

    fix: remove nvim-tree, Oil is the best.

--- nvim/lua/config/browser.lua
+++ nvim/lua/config/browser.lua
@@ -1,46 +1,4 @@
-vim.keymap.set("n", "<C-j>", ":NvimTreeFindFile<CR>")
-vim.keymap.set("n", "<Leader>e", ":NvimTreeToggle<CR>")
-
-vim.keymap.set("n", "<Leader>.", ":Oil<CR>")
-
-local function my_on_attach(bufnr)
-    local api = require("nvim-tree.api")
-
-    local function opts(desc)
-        return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-    end
-
-    -- use all default mappings
-    api.config.mappings.default_on_attach(bufnr)
-
-    -- remove a default
-    vim.keymap.del("n", "<C-e>", { buffer = bufnr })
-end
-
-require("nvim-tree").setup({
-    sort = {
-        sorter = "case_sensitive",
-    },
-    view = {
-        width = 44,
-    },
-    renderer = {
-        group_empty = true,
-    },
-    filters = {
-        dotfiles = false,
-    },
-    diagnostics = {
-        enable = true,
-        show_on_dirs = true,
-        show_on_open_dirs = true,
-        debounce_delay = 500,
-    },
-    ---
-    reload_on_bufenter = true,
-    on_attach = my_on_attach,
-    ---
-})
+vim.keymap.set("n", "<C-j>", ":Oil<CR>")
 
 vim.api.nvim_create_autocmd("QuitPre", {
     callback = function()
