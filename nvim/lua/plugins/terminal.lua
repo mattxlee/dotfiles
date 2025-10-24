99157c2fd1300727257c6a6ceb2efd7f30513c86
MATTHΞW <mattxlee@gmail.com>
2025-10-24 20:28:00 +0900

    fix: use split with direction horizontal for terminal.

--- nvim/lua/plugins/terminal.lua
+++ nvim/lua/plugins/terminal.lua
@@ -1,12 +1,19 @@
 return {
     {
-        { 'akinsho/toggleterm.nvim', version = "*",
-            config = {
-                open_mapping = [[<Leader>tt]], -- Shortcut to open/close terminal
-                insert_mappings = false,   -- Apply mapping in insert mode
-                terminal_mappings = true, -- Apply mapping in terminal mode
-                direction = "float",
-            }
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = {
+            open_mapping = [[<Leader>tt]],     -- Shortcut to open/close terminal
+            insert_mappings = false,           -- Apply mapping in insert mode
+            terminal_mappings = true,          -- Apply mapping in terminal mode
+            direction = "horizontal",
+            size = function(term)
+                if term.direction == "horizontal" then
+                    return vim.o.lines * 0.5
+                elseif term.direction == "vertical" then
+                    return vim.o.columns * 0.5
+                end
+            end,
         }
     },
 }
