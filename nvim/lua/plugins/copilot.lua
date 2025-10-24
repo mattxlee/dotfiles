5b71d2f618e101a1ea9a6554f5a81318f400aae2
MATTHΞW <mattxlee@gmail.com>
2025-10-24 19:51:47 +0900

    fix: turn auto-complete from copilot on.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -6,8 +6,8 @@ return {
         config = function()
             require("copilot").setup({
                 suggestion = {
-                    enabled = false,
-                    auto_trigger = false,
+                    enabled = true,
+                    auto_trigger = true,
                     hide_during_completion = true,
                     debounce = 75,
                     trigger_on_accept = true,
