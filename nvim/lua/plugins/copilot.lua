042af1347cc7b831be15a3f180795875f990ceec
MATTHΞW <mattxlee@gmail.com>
2025-10-31 19:06:33 +0900

    fix: turn auto-suggestion off of the copilot.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -7,7 +7,7 @@ return {
             require("copilot").setup({
                 suggestion = {
                     enabled = true,
-                    auto_trigger = true,
+                    auto_trigger = false,
                     hide_during_completion = true,
                     debounce = 75,
                     trigger_on_accept = true,
