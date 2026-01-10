f78e763bce8fbc8eb9f5ff2fba6849eb133326ee
MATTHΞW <mattxlee@gmail.com>
2026-01-11 01:30:49 +0800

    feat: allow float terminal window with <leader>tf.

--- nvim/lua/config/terminal.lua
+++ nvim/lua/config/terminal.lua
@@ -1,2 +1,3 @@
 vim.keymap.set("n", "<Leader>ts", ":ToggleTerm direction=horizontal<CR>")
 vim.keymap.set("n", "<Leader>tv", ":ToggleTerm direction=vertical<CR>")
+vim.keymap.set("n", "<Leader>tf", ":ToggleTerm direction=float<CR>")
