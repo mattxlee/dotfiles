6c27fd7b5945b778d0458235b2974eb3e6760914
MATTHΞW <mattxlee@gmail.com>
2026-01-13 15:41:09 +0800

    Remove copilot chat.

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -7,7 +7,7 @@ return {
             require("copilot").setup({
                 suggestion = {
                     enabled = true,
-                    auto_trigger = false,
+                    auto_trigger = true,
                     hide_during_completion = true,
                     debounce = 75,
                     trigger_on_accept = true,
@@ -27,23 +27,5 @@ return {
                 panel = { enabled = false },
             })
         end,
-    },
-    {
-        "CopilotC-Nvim/CopilotChat.nvim",
-        dependencies = {
-            { "nvim-lua/plenary.nvim", branch = "master" },
-        },
-        build = "make tiktoken",
-        opts = {
-            model = "gpt-4.1",    -- AI model to use
-            temperature = 0.1,    -- Lower = focused, higher = creative
-            window = {
-                layout = "float", -- 'vertical', 'horizontal', 'float'
-                width = 0.9,
-                height = 0.9,
-                border = "rounded",
-            },
-            auto_insert_mode = false,
-        },
     },
 }
