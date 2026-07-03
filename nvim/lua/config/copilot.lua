3021c86d83862c3cbf6cfa4895eb2f5212dd13f0
MATTHΞW <mattxlee@gmail.com>
2026-07-03 22:22:14 +0800

    Add new shortcut c-e to complete a word from copilot suggestion.

--- nvim/lua/config/copilot.lua
+++ nvim/lua/config/copilot.lua
@@ -2,4 +2,5 @@ vim.keymap.set('i', '<c-y>', 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false
 })
+vim.keymap.set('i', '<C-e>', '<Plug>(copilot-accept-word)')
 vim.g.copilot_no_tab_map = true
