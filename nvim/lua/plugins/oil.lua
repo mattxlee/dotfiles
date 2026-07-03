a9e5f640e50cc4086e96cd3b49d84d7ab7061465
MATTHΞW <mattxlee@gmail.com>
2026-07-03 13:41:37 +0800

    Show hidden files in oil and remove the shortcuts for opening with split pane.

--- nvim/lua/plugins/oil.lua
+++ nvim/lua/plugins/oil.lua
@@ -4,8 +4,13 @@ return {
         opts = {
             columns = {},
             keymaps = {
+                ["<C-s>"] = false,
+                ["<C-h>"] = false,
                 ["<C-p>"] = false,
             },
+        },
+        view_options = {
+            show_hidden = true,
         },
         -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
         lazy = false,
