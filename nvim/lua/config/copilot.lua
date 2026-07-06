70f74f30ae05dd0eb127b99a1be3a10e7166a751
MATTHΞW <mattxlee@gmail.com>
2026-07-06 10:26:29 +0800

    Change the shortcuts for how to complete the suggestion.

--- nvim/lua/config/copilot.lua
+++ nvim/lua/config/copilot.lua
@@ -2,5 +2,6 @@ vim.keymap.set('i', '<c-y>', 'copilot#Accept("\\<CR>")', {
     expr = true,
     replace_keycodes = false
 })
-vim.keymap.set('i', '<C-e>', '<Plug>(copilot-accept-word)')
+vim.keymap.set('i', '<C-e>', '<Plug>(copilot-accept-line)')
+vim.keymap.set('i', '<C-f>', '<Plug>(copilot-accept-word)')
 vim.g.copilot_no_tab_map = true
