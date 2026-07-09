49bf842ce0042dfbf00115a2ab47bab36a077884
MATTHΞW <mattxlee@gmail.com>
2026-07-09 11:24:28 +0800

    Improve config for fzf and kitty font size/theme.

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -23,19 +23,19 @@ return {
                     fzf_colors = {
                         true, -- inherit fzf colors that aren"t specified below from
                         -- the auto-generated theme similar to `fzf_colors=true`
-                        ["fg"]      = { "fg", "CursorLine" },
-                        ["bg"]      = { "bg", "Normal" },
-                        ["hl"]      = { "fg", "Comment" },
+                        -- ["fg"]      = { "fg", "CursorLine" },
+                        -- ["bg"]      = { "bg", "Normal" },
+                        -- ["hl"]      = { "fg", "Comment" },
                         ["fg+"]     = { "fg", "Normal" },
-                        ["bg+"]     = { "bg", { "CursorLine", "Normal" } },
-                        ["hl+"]     = { "fg", "Statement" },
-                        ["info"]    = { "fg", "PreProc" },
-                        ["prompt"]  = { "fg", "Conditional" },
-                        ["pointer"] = { "fg", "Exception" },
-                        ["marker"]  = { "fg", "Keyword" },
-                        ["spinner"] = { "fg", "Label" },
-                        ["header"]  = { "fg", "Comment" },
-                        ["gutter"]  = "-1",
+                        ["bg+"]     = { "bg", "Normal" },
+                        -- ["hl+"]     = { "fg", "Statement" },
+                        -- ["info"]    = { "fg", "PreProc" },
+                        -- ["prompt"]  = { "fg", "Conditional" },
+                        -- ["pointer"] = { "fg", "Exception" },
+                        -- ["marker"]  = { "fg", "Keyword" },
+                        -- ["spinner"] = { "fg", "Label" },
+                        -- ["header"]  = { "fg", "Comment" },
+                        -- ["gutter"]  = "-1",
                     },
                 }
 
