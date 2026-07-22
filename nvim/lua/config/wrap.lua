733690d5bcd43f186bc2bdacfa18dd8d48d0350e
MATTHΞW <mattxlee@gmail.com>
2026-07-22 18:21:44 +0800

    Fix the breakat make sure that nvim won't wrap single word.

--- nvim/lua/config/wrap.lua
+++ nvim/lua/config/wrap.lua
@@ -1,7 +1,7 @@
 vim.api.nvim_create_autocmd("FileType", {
     pattern = "markdown",
     callback = function()
-        vim.opt_local.breakat = ""
+        vim.opt_local.breakat = " \t!@*-+;:,./?"
         vim.cmd(":Wrapwidth 80")
     end,
 })
