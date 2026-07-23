73dc51ab076fb4c0b4822dd47ca1b16d9e13fa17
MATTHΞW <mattxlee@gmail.com>
2026-07-23 18:55:40 +0800

    Optimize the lsp by removing the fallback of shortcuts <c-f>, <c-b>.

--- nvim/lua/config/copilot.lua
+++ nvim/lua/config/copilot.lua
@@ -3,5 +3,5 @@ vim.keymap.set('i', '<c-y>', 'copilot#Accept("\\<CR>")', {
     replace_keycodes = false
 })
 vim.keymap.set('i', '<C-e>', '<Plug>(copilot-accept-line)')
-vim.keymap.set('i', '<C-f>', '<Plug>(copilot-accept-word)')
+vim.keymap.set('i', '<C-n>', '<Plug>(copilot-accept-word)')
 vim.g.copilot_no_tab_map = true
