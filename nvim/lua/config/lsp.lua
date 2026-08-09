5149cae0af291d240bac48b8066bbf410fa52e63
MATTHΞW <mattxlee@gmail.com>
2026-08-09 15:08:06 +0800

    Add LspInfo.

--- nvim/lua/config/lsp.lua
+++ nvim/lua/config/lsp.lua
@@ -8,6 +8,12 @@ vim.keymap.set("n", "<leader>cl", function()
     vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
 end, { desc = "Toggle inlay hints" })
 
+if vim.fn.exists(":LspInfo") == 0 then
+    vim.api.nvim_create_user_command("LspInfo", function()
+        vim.cmd("checkhealth vim.lsp")
+    end, { desc = "Show LSP client and configuration information" })
+end
+
 -- format code
 require("conform").setup({
     formatters_by_ft = {
